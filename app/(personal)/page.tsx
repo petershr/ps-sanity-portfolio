76874b232661e8a787b62628d3a1c670ad69b6fa
petershr <peterpshr@gmail.com>
2026-04-13 13:40:25 -0700

    removed next steps default footer

--- app/(personal)/page.tsx
+++ app/(personal)/page.tsx
@@ -1,23 +1,10 @@
-import {HomePage} from '@/components/HomePage'
-import {studioUrl} from '@/sanity/lib/api'
-import {sanityFetch} from '@/sanity/lib/live'
-import {homePageQuery} from '@/sanity/lib/queries'
+import { HomePage } from '@/components/HomePage'
+import { studioUrl } from '@/sanity/lib/api'
+import { sanityFetch } from '@/sanity/lib/live'
+import { homePageQuery } from '@/sanity/lib/queries'
 import Link from 'next/link'
 
 export default async function IndexRoute() {
-  const {data} = await sanityFetch({query: homePageQuery})
-
-  if (!data) {
-    return (
-      <div className="text-center">
-        You don&rsquo;t have a homepage yet,{' '}
-        <Link href={`${studioUrl}/structure/home`} className="underline">
-          create one now
-        </Link>
-        !
-      </div>
-    )
-  }
-
+  const { data } = await sanityFetch({ query: homePageQuery })
   return <HomePage data={data} />
 }
