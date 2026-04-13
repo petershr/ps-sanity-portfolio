6b0b36ff63445134bffb3ad15a7f947c681ab553
petershr <peterpshr@gmail.com>
2026-04-13 13:49:34 -0700

    now got rid of next steps with intro template

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -1,24 +1,23 @@
 import '@/styles/index.css'
-import {CustomPortableText} from '@/components/CustomPortableText'
-import {Navbar} from '@/components/Navbar'
-import IntroTemplate from '@/intro-template'
-import {sanityFetch, SanityLive} from '@/sanity/lib/live'
-import {homePageQuery, settingsQuery} from '@/sanity/lib/queries'
-import {urlForOpenGraphImage} from '@/sanity/lib/utils'
-import {SpeedInsights} from '@vercel/speed-insights/next'
-import type {Metadata, Viewport} from 'next'
-import {toPlainText, type PortableTextBlock} from 'next-sanity'
-import {VisualEditing} from 'next-sanity/visual-editing'
-import {draftMode} from 'next/headers'
-import {Suspense} from 'react'
-import {Toaster} from 'sonner'
-import {handleError} from './client-functions'
-import {DraftModeToast} from './DraftModeToast'
+import { CustomPortableText } from '@/components/CustomPortableText'
+import { Navbar } from '@/components/Navbar'
+import { sanityFetch, SanityLive } from '@/sanity/lib/live'
+import { homePageQuery, settingsQuery } from '@/sanity/lib/queries'
+import { urlForOpenGraphImage } from '@/sanity/lib/utils'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import type { Metadata, Viewport } from 'next'
+import { toPlainText, type PortableTextBlock } from 'next-sanity'
+import { VisualEditing } from 'next-sanity/visual-editing'
+import { draftMode } from 'next/headers'
+import { Suspense } from 'react'
+import { Toaster } from 'sonner'
+import { handleError } from './client-functions'
+import { DraftModeToast } from './DraftModeToast'
 
 export async function generateMetadata(): Promise<Metadata> {
-  const [{data: settings}, {data: homePage}] = await Promise.all([
-    sanityFetch({query: settingsQuery, stega: false}),
-    sanityFetch({query: homePageQuery, stega: false}),
+  const [{ data: settings }, { data: homePage }] = await Promise.all([
+    sanityFetch({ query: settingsQuery, stega: false }),
+    sanityFetch({ query: homePageQuery, stega: false }),
   ])
 
   // @ts-ignore the image type sometimes fails
@@ -26,9 +25,9 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: homePage?.title
       ? {
-          template: `%s | ${homePage.title}`,
-          default: homePage.title || 'Personal website',
-        }
+        template: `%s | ${homePage.title}`,
+        default: homePage.title || 'Personal website',
+      }
       : undefined,
     description: homePage?.overview ? toPlainText(homePage.overview) : undefined,
     openGraph: {
@@ -41,8 +40,8 @@ export const viewport: Viewport = {
   themeColor: '#000',
 }
 
-export default async function IndexRoute({children}: {children: React.ReactNode}) {
-  const {data} = await sanityFetch({query: settingsQuery})
+export default async function IndexRoute({ children }: { children: React.ReactNode }) {
+  const { data } = await sanityFetch({ query: settingsQuery })
   return (
     <>
       <div className="flex min-h-screen flex-col text-black animated-gradient-bg">
@@ -53,9 +52,6 @@ export default async function IndexRoute({children}: {children: React.ReactNode}
           <a href="https://www.linkedin.com/in/peter-shr/" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-[#0b66c2] font-semibold text-lg md:text-xl interactive-hover">LinkedIn</a>
           <a href="https://github.com/petershr" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-black font-semibold text-lg md:text-xl interactive-hover">GitHub</a>
         </footer>
-        <Suspense>
-          <IntroTemplate />
-        </Suspense>
       </div>
       <Toaster />
       <SanityLive onError={handleError} />
